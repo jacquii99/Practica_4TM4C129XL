661120a6586a5ce5cfa2c4f0f64977041820bc04
jacquii99 <jmendez21@alumnos.uaq.mx>
2023-01-29 00:09:09 -0600

    tyjwe

--- ADC.c
+++ ADC.c
@@ -10,7 +10,7 @@ extern void Configura_Reg_ADC0(void)
 {
     /*
     Habilitar el modulo 0 del ADC con seis canales analogicos 
-    en los puertos E, D Y B a una velocidad de conversion de 2Msps
+    en los puertos E, D Y B a una velocidad de conversion de 2Msps con evento de procesador 
     con los siguientes secuenciadores: sec3,sec0y sec2 
     */
      //Pag 396 para inicializar el modulo de reloj del adc RCGCADC
@@ -43,9 +43,9 @@ extern void Configura_Reg_ADC0(void)
     //Pag 1099 Este registro (ADCSSPRI) configura la prioridad de los secuenciadores
     ADC0->SSPRI = 0x0231;//secuenciador0 prioridad más alta y el secuenciador 3 prioridad más baja
     //Pag 1077 (ADCACTSS) Este registro controla la activación de los secuenciadores
-    ADC0->ACTSS  =   (1<<0) | (1<<2) | (1<<3) | (0<<1);
+    ADC0->ACTSS  =   (0<<0) | (0<<2) | (0<<3) | (0<<1);
     //Pag 1091 Este registro (ADCEMUX) selecciona el evento que activa la conversión (trigger)
-    ADC0->EMUX  = (0x0000);
+    ADC0->EMUX  = (0x0000); //por procesador 
     //Pag 1129 Este registro (ADCSSMUX2) define las entradas analógicas con el canal y secuenciador seleccionado
     ADC0->SSMUX2 = 0x0089;
     //pag 868 Este registro (ADCSSCTL2), configura el bit de control de muestreo y la interrupción
