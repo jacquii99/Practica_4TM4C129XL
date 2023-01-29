3c2af39ca05c92212a0e44ebb99aec6aa7a2c779
jacquii99 <jmendez21@alumnos.uaq.mx>
2023-01-28 23:35:26 -0600

    listo

--- ADC.c
+++ ADC.c
@@ -9,10 +9,9 @@
 extern void Configura_Reg_ADC0(void)
 {
     /*
-    Habilitar el modulo 0 del ADC con dos canales analogicos 
-    en el puerto E a una velocidad de conversion de 250ksps
-    dandole la mayor prioridad al secuenciador 2 con evento
-    de procesador 
+    Habilitar el modulo 0 del ADC con seis canales analogicos 
+    en los puertos E, D Y B a una velocidad de conversion de 250ksps
+    con los siguientes ecuenciadores: sec3,sec0y sec2 
     */
      //Pag 396 para inicializar el modulo de reloj del adc RCGCADC
     SYSCTL->RCGCADC = (1<<0); //modulo 0 del ADC 
@@ -32,9 +31,9 @@ extern void Configura_Reg_ADC0(void)
     GPIOD_AHB->DEN = (1<<7) | (1<<5) | (1<<4); 
     GPIOB_AHB->DEN = (1<<5);
     //Pag 787 GPIOPCTL registro combinado con el GPIOAFSEL y la tabla pag 1808
-    GPIOE_AHB->PCTL = GPIOE_AHB->PCTL & (0xFF00FFFF);
+    GPIOE_AHB->PCTL = GPIOE_AHB->PCTL & (0xFFF00FFF);
     GPIOD_AHB->PCTL = GPIOD_AHB->PCTL & (0xF0F00FFF);
-    GPIOB_AHB->PCTL = GPIOB_AHB->PCTL & (0xFF0FFFFF);
+    GPIOB_AHB->PCTL = GPIOB_AHB->PCTL & (0xFFF0FFFF);
     //(GPIOAMSEL) pag.786 habilitar analogico
     GPIOE_AHB->AMSEL = (1<<1) | (1<<4);
     GPIOD_AHB->AMSEL = (1<<4) | (1<<5) | (1<<7);
@@ -42,9 +41,9 @@ extern void Configura_Reg_ADC0(void)
     //Pag 1159 El registro (ADCPC) establece la velocidad de conversión por segundo
     ADC0->PC = 0x7;//250ksps
     //Pag 1099 Este registro (ADCSSPRI) configura la prioridad de los secuenciadores
-    ADC0->SSPRI = 0x0231;
+    ADC0->SSPRI = 0x0231;//secuenciador0 prioridad más alta y el secuenciador 3 prioridad más baja
     //Pag 1077 (ADCACTSS) Este registro controla la activación de los secuenciadores
-    ADC0->ACTSS  =   (0<<2) | (0<<3) | (0<<1);
+    ADC0->ACTSS  =   (1<<0) | (1<<2) | (1<<3) | (0<<1);
     //Pag 1091 Este registro (ADCEMUX) selecciona el evento que activa la conversión (trigger)
     ADC0->EMUX  = (0x0000);
     //Pag 1129 Este registro (ADCSSMUX2) define las entradas analógicas con el canal y secuenciador seleccionado
@@ -56,7 +55,7 @@ extern void Configura_Reg_ADC0(void)
     //NVIC_PRI4_R = (NVIC_PRI4_R & 0xFFFFFF00) | 0x00000020;
     //NVIC_EN0_R = 0x00010000;
     //Pag 1077 (ADCACTSS) Este registro controla la activación de los secuenciadores
-    ADC0->ACTSS = (0<<3) | (1<<2) | (0<<1) | (0<<0);
+    ADC0->ACTSS = (1<<3) | (1<<2) | (0<<1) | (1<<0);
     ADC0->PSSI |= (1<<2);
 }
 extern void ADC0_InSeq2(uint16_t *Result, uint16_t*duty){
